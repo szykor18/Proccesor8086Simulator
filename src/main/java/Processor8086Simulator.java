16b4f4b5c568dac4bfb02f2744e53007d5b479c8
szykor18 <szymonkornik.bs@gmail.com>
2023-11-19 18:17:15 +0100

    refaktoryzacja

--- src/main/java/Processor8086Simulator.java
+++ src/main/java/Processor8086Simulator.java
@@ -1,15 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Processor8086Simulator {
     // Rejestry
-    int AX, BX, CX, DX, SI, DI, BP, SP, IP;
-    int[] FLAGS = new int[16]; // Przykładowo, każdy bit reprezentuje inny flag
+    private Map<String, Integer> registers;
+    public Processor8086Simulator() {
+        // Inicjalizacja rejestrów
+        registers = new HashMap<>();
+        registers.put("AX", 0);
+        registers.put("BX", 0);
+        registers.put("CX", 0);
+        registers.put("DX", 0);
+    }
 
     // Metoda do wykonywania operacji MOV
-    void MOV(String dest, int value) {
-        switch (dest) {
-            case "AX":
-                AX = value;
-                break;
-            // inne przypadki dla BX, CX, DX itd.
+    public void MOV(String dest, int value) {
+        if (registers.containsKey(dest)) {
+            registers.put(dest, value);
+        } else {
+            System.out.println("Nieznany rejestr: " + dest);
         }
     }
+
 }
