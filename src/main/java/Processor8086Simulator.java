2a19f2fad759bb02e54780648ec785811d784e6f
szykor18 <szymonkornik.bs@gmail.com>
2023-11-19 18:15:02 +0100

    metoda MOV

--- src/main/java/Processor8086Simulator.java
+++ src/main/java/Processor8086Simulator.java
@@ -2,4 +2,14 @@ public class Processor8086Simulator {
     // Rejestry
     int AX, BX, CX, DX, SI, DI, BP, SP, IP;
     int[] FLAGS = new int[16]; // Przykładowo, każdy bit reprezentuje inny flag
+
+    // Metoda do wykonywania operacji MOV
+    void MOV(String dest, int value) {
+        switch (dest) {
+            case "AX":
+                AX = value;
+                break;
+            // inne przypadki dla BX, CX, DX itd.
+        }
+    }
 }
