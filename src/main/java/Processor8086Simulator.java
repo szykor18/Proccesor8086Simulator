800f5ecb272f64eb7f92f5c137474293287fa6ea
szykor18 <szymonkornik.bs@gmail.com>
2023-11-19 18:11:17 +0100

    dodanie rejestrów

--- src/main/java/Processor8086Simulator.java
+++ src/main/java/Processor8086Simulator.java
@@ -1,6 +1,5 @@
-public class Main {
-
-    public static void main(String[] args) {
-        
-    }
+public class Processor8086Simulator {
+    // Rejestry
+    int AX, BX, CX, DX, SI, DI, BP, SP, IP;
+    int[] FLAGS = new int[16]; // Przykładowo, każdy bit reprezentuje inny flag
 }
