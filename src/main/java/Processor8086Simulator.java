a2493d108159add9f990b9afed24e4ff84c0262f
szykor18 <szymonkornik.bs@gmail.com>
2023-11-19 18:19:04 +0100

    metoda SUB

--- src/main/java/Processor8086Simulator.java
+++ src/main/java/Processor8086Simulator.java
@@ -30,5 +30,13 @@ public class Processor8086Simulator {
             System.out.println("Nieznany rejestr: " + dest);
         }
     }
+    // Metoda do operacji SUB
+    public void SUB(String dest, int value) {
+        if (registers.containsKey(dest)) {
+            int originalValue = registers.get(dest);
+            registers.put(dest, originalValue - value);
+        } else {
+            System.out.println("Nieznany rejestr: " + dest);
+        }
 
 }
