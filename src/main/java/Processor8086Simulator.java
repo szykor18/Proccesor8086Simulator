b16bd63af1603c6ef2ba76c3d0b980fb07fb0b0b
szykor18 <szymonkornik.bs@gmail.com>
2023-11-19 18:17:39 +0100

    metoda ADD

--- src/main/java/Processor8086Simulator.java
+++ src/main/java/Processor8086Simulator.java
@@ -21,5 +21,14 @@ public class Processor8086Simulator {
             System.out.println("Nieznany rejestr: " + dest);
         }
     }
+    // Metoda do operacji ADD
+    public void ADD(String dest, int value) {
+        if (registers.containsKey(dest)) {
+            int originalValue = registers.get(dest);
+            registers.put(dest, originalValue + value);
+        } else {
+            System.out.println("Nieznany rejestr: " + dest);
+        }
+    }
 
 }
